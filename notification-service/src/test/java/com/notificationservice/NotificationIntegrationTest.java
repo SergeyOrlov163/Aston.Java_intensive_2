9b97877e2793364ebdf8c0000e943fc10e05641f
Sergey Orlov <graforlov163gmail.com>
2025-10-03 21:58:49 +0400

    5.1_Kafka

--- notification-service/src/test/java/com/notificationservice/NotificationIntegrationTest.java
+++ notification-service/src/test/java/com/notificationservice/NotificationIntegrationTest.java
@@ -1,13 +1,17 @@
 package com.notificationservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.notificationservice.controller.NotificationController;
 import com.notificationservice.dto.NotificationRequestDto;
 import com.notificationservice.service.NotificationService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -15,15 +19,17 @@ import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureWebMvc
+@WebMvcTest(NotificationController.class)
 public class NotificationIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockitoBean
-    private NotificationService notificationService;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private com.notificationservice.service.NotificationService notificationService;
 
     @Test
     void sendNotification_viaApi_callsService() throws Exception {
@@ -33,12 +39,7 @@ public class NotificationIntegrationTest {
 
         mockMvc.perform(post("/api/notifications/send")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("""
-                            {
-                                "email": "test@example.com",
-                                "operation": "CREATE"
-                            }
-                            """))
+                        .content(objectMapper.writeValueAsString(requestDto)))
                         .andExpect(status().isOk());
 
         verify(notificationService).sendNotification("test@example.com", "CREATE");
